8b7510eeac38b4cb692e4508d3d08b00adc9136b
Lina María Ureña Bolívar <lina8maria8@hotmail.com>
2022-01-29 11:37:43 -0500

    Strings files

---
+++ StringExample/StringExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name.");
+            string fullUserName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + fullUserName + ", welcome!");
+
+        }
+    }
+}
